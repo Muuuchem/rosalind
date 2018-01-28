c72fc99e767a5e5eab9101a235903653755f535b
Tyler Eakes <teakesprofessional@gmail.com>
2018-01-28 15:33:36 -0800

    first two

---
+++ rna.py
@@ -0,0 +1,15 @@
+
+Sample_DNA = 'GATGGAACTTGACTACGTAAATT'
+
+def dna_to_rna(dna_seq):
+    final = []
+    characters = list(dna_seq)
+    for i in characters:
+        if i != 'T':
+            final.append(i)
+        else:
+            final.append('U')
+
+    print(''.join(final))
+if __name__ == '__main__':
+    dna_to_rna(problem_set)
